18b0f25b267146776a3975acc32a18bcc8c66839
madawei2699 <madawei2699@gmail.com>
2020-01-12 23:43:41 +0800

    add rule 1 && 2 test

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,7 +1,24 @@
 package com.thoughtworks;
 
 public class FizzBuzz {
-    public String handleDigit(int digit) {
-        return "Fizz";
+
+    public static void main(String[] args) {
+        for( int i = 0; i < 120; i++) {
+            String result = fizzBuzz(i+1);
+            System.out.println(result);
+        }
+    }
+
+    public static String fizzBuzz(int digit) {
+        if (digit % 3 == 0) {
+            return "Fizz";
+        }
+        if (digit % 5 == 0) {
+            return "Buzz";
+        }
+        if (digit % 7 == 0) {
+            return "Whizz";
+        }
+        return String.valueOf(digit);
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -7,12 +7,38 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     @Test
+    public void should_return_origin_string_when_given_input_is_not_match_any_rule() {
+        // given
+        // when
+        String actual = FizzBuzz.fizzBuzz(2);
+        // then
+        assertEquals("2", actual);
+    }
+
+    @Test
     public void should_return_Fizz_when_given_input_can_be_divided_by_3() {
         // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
         // when
-        String actual = fizzBuzz.handleDigit(3);
+        String actual = FizzBuzz.fizzBuzz(3);
         // then
-        assertEquals(actual, "Fizz");
+        assertEquals("Fizz", actual);
+    }
+
+    @Test
+    public void should_return_Buzz_when_given_input_can_be_divided_by_5() {
+        // given
+        // when
+        String actual = FizzBuzz.fizzBuzz(5);
+        // then
+        assertEquals("Buzz", actual);
+    }
+
+    @Test
+    public void should_return_Whizz_when_given_input_can_be_divided_by_7() {
+        // given
+        // when
+        String actual = FizzBuzz.fizzBuzz(7);
+        // then
+        assertEquals("Whizz", actual);
     }
 }
